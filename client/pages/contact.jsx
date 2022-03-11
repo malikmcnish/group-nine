2fb078e36640d4cd1330f91ef8d4984829643b1f
malikmcnish <35948002+malikmcnish@users.noreply.github.com>
2022-03-11 01:29:20 -0500

    contact page
    updates on contact page

--- client/pages/contact.jsx
+++ client/pages/contact.jsx
@@ -1,9 +1,81 @@
-import React from "react";
+import React from 'react'
 
 export default function ContactPage() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <div className="container">
+        <strong
+          style={{
+            color: 'red',
+            fontSize: '20pt',
+            marginLeft: '48%',
+          }}
+        >
+          Contact us
+        </strong>
+
+        <form
+          className="form"
+          style={{
+            padding: '12px 20px',
+            marginRight: '50%',
+            borderRadius: '5px',
+          }}
+        >
+          <div className="name">
+            <label for="firstName">First Name</label>
+            <input
+              style={{ borderRadius: '5px' }}
+              type="text"
+              name="firstName"
+              className="firstName"
+              tabIndex="1"
+              placeholder="First name..."
+            />
+            <label for="lastName">Last Name</label>
+            <input
+              style={{ borderRadius: '5px' }}
+              type="text"
+              name="lastName"
+              className="lastName"
+              tabIndex="2"
+              placeholder="Last name..."
+            />
+
+            <label for="email">Email</label>
+            <input
+              style={{ borderRadius: '5px' }}
+              type="email"
+              name="email"
+              id="email"
+              className="email"
+              placeholder="example@corp.com"
+              tabIndex="3"
+            />
+
+            <label for="message">Message</label>
+            <textarea
+              style={{ borderRadius: '5px' }}
+              placeholder="Start typing..."
+              className="message"
+              name="message"
+            />
+
+            <button
+              type="submit"
+              className="send"
+              style={{
+                border: '1px solid green',
+                background: 'green',
+                borderRadius: '5px',
+                marginTop: '10px',
+              }}
+            >
+              Submit Form
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
